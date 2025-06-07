d4d2976ee0b7681de82bac32c8d4f8df9d57e346
Agustina Pucharras <pucharra81@gmail.com>
2025-06-07 01:53:28 -0300

    agregue contenido a contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,99 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import { useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "../css/Contact.css";
+
 
 const Contact = () => {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState(null)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Thank you for contacting me, ${name}!😃 . I will get back to you at ${email} or ${phone}.`)
+    e.target.reset()
+  };
+
+
   return (
     <div>
-      <Header/>
-    </div>
-  )
-}
+      <Header />
+      <h2 className="aboutMe text-center">CONTACT ME</h2>
+      <div className="mt-4" >
+        <Form onSubmit={handleSubmit}>
 
-export default Contact
+          <div
+            className="container mt-3"
+            controlId="formBasicEmail"
+          >
+            <div className="d-flex gap-2 justify-content-evenly">
+              <Form.Label style={{color:"white"}}>Name</Form.Label>
+            </div>
+            <div className="d-flex gap-2 justify-content-evenly">
+              <Form.Control
+                type="name"
+                style={{ width: "30%" }}
+                placeholder="Your name"
+                name="name"
+                onChange={(e)=>setName(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+
+          <div
+            className="container mt-3"
+            controlId="formBasicEmail"
+          >
+            <div className="d-flex gap-2 justify-content-evenly">
+              <Form.Label style={{color:"white"}}>Email address</Form.Label>
+            </div>
+            <div className="d-flex gap-2 justify-content-evenly">
+              <Form.Control
+                type="email"
+                style={{ width: "30%" }}
+                placeholder="Your email"
+                name="email"
+                onChange={(e)=>setEmail(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+
+          <div
+            className="container mt-3"
+            controlId="formBasicEmail"
+          >
+            <div className="d-flex gap-2 justify-content-evenly">
+              <Form.Label style={{color:"white"}}>Phone number</Form.Label>
+            </div>
+            <div className="d-flex gap-2 justify-content-evenly">
+              <Form.Control
+                type="tel"
+                style={{ width: "30%" }}
+                placeholder="Your phone number"
+                name="phone"
+                onChange={(e)=>setPhone(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+
+          <div className="d-flex justify-content-center">
+            <Button type="submit" className="btnContact mt-4">
+              Send
+            </Button>
+          </div>
+        </Form>
+      </div>
+  
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,11 +8,11 @@ const Projects = () => {
     <div>
       <Header />
       <h2 className="aboutMe text-center">MY PROJECTS</h2>
-      <p>
+      <h5 className="text-center text-light mt-5 mb-5 ">
         Here you can find some of the projects I’ve worked on. Each one reflects
         my passion for coding, problem-solving, and continuous learning. Feel
         free to explore them!
-      </p>
+      </h5>
       <CardProjects />
       <Footer />
     </div>
