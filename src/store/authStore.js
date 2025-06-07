22dad9b0f45eaa47e54671c129288d6f919e9c1b
Agustina Pucharras <pucharra81@gmail.com>
2025-06-07 17:15:54 -0300

    eliminacion de elementos redundantes

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,9 +1,9 @@
 import { create } from 'zustand';
 const useThemeStore = create((set) => ({
-  // Estado inicial en modo oscuro activo
-  background: true, // true = modo oscuro, false = claro
-  color: true,      // true = texto claro (modo oscuro), false texto oscuro (modo claro)
-  // Método para alternar ambos a la vez
+
+  background: true, 
+  color: true,     
+  
   toggleTheme: () =>
     set((state) => ({
       background: !state.background,
